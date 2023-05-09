9b7442b6c05bd1724765f368d81f943d322e46f6
Erik Högberg <ErikHogberg@users.noreply.github.com>
2023-05-09 10:56:43 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿namespace ClassLibrary1
+{
+	public class Class1
+	{
+		enum Operand
+		{
+			None,
+			Plus,
+			Minus,
+			Multiply,
+			Divide,
+		}
+
+		public static void Two()
+		{
+			Console.WriteLine("Hello, World!");
+		}
+
+		public static float NewMethod(string svar)
+		{
+			float lastNumber = 0;
+			Operand lastOperand = Operand.Plus;
+
+			foreach (char bokstav in svar)
+			{
+
+				float nummer;
+				if (float.TryParse(bokstav.ToString(), out nummer))
+				{
+					lastNumber = lastOperand switch
+					{
+						Operand.Plus => lastNumber + nummer,
+						Operand.Minus => lastNumber - nummer,
+						Operand.Multiply => lastNumber * nummer,
+						Operand.Divide => lastNumber / nummer,
+						_ => lastNumber,
+					};
+				}
+				else
+				{
+					lastOperand = bokstav.ToString() switch
+					{
+						"+" => Operand.Plus,
+						"-" => Operand.Minus,
+						"*" => Operand.Multiply,
+						"/" => Operand.Divide,
+						_ => Operand.None,
+					};
+				}
+			}
+
+			return lastNumber;
+		}
+	}
+}
