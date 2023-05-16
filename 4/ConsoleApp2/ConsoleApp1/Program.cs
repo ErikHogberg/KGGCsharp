eb1ac8cf3869ae4dbebada3f617300e3f68d5596
Erik Högberg <erik@e.email>
2023-05-16 13:49:12 +0200

    rektangel

--- 4/ConsoleApp2/ConsoleApp1/Program.cs
+++ 4/ConsoleApp2/ConsoleApp1/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using ClassLibrary1;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+using MonoGame.Extended.SceneGraphs;
 
 class Program
 {
@@ -8,14 +11,30 @@ class Program
     class MyClass: Game
     {
 		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+
         public MyClass()
         {
 			graphics = new GraphicsDeviceManager(this);
 		}
 
+		protected override void LoadContent()
+		{
+			spriteBatch = new(GraphicsDevice);
+
+
+			base.LoadContent();
+		}
+
 		protected override void Draw(GameTime gameTime)
 		{
 			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			spriteBatch.Begin();
+
+			spriteBatch.DrawRectangle(new(10, 10, 100, 100), Color.Wheat);
+			spriteBatch.End();
 
 			base.Draw(gameTime);
 		}
