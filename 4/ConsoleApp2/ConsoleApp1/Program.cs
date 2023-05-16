49836ba22a9ae2c02a9806d2847020584d585c14
Erik Högberg <erik@e.email>
2023-05-16 12:30:37 +0200

    monogame

--- 4/ConsoleApp2/ConsoleApp1/Program.cs
+++ 4/ConsoleApp2/ConsoleApp1/Program.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using ClassLibrary1;
+using Microsoft.Xna.Framework;
 
 class Program
 {
-	
+
+    class MyClass: Game
+    {
+		GraphicsDeviceManager graphics;
+        public MyClass()
+        {
+			graphics = new GraphicsDeviceManager(this);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			base.Draw(gameTime);
+		}
+	}
+
 	static void Main(string[] args)
 	{
+
+		new MyClass().Run();
+
+
 		Class1.Two();
 
 
